3fc094b117e41425068d72989f15633d39a86767
DevinKin <DevinKin@163.com>
2017-12-25 22:12:11 +0800

    remove

---
+++ fo/removeif1.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: removeif1.cpp
+    > Author: King
+    > Mail: arturiapendragon_1@163.com 
+    > Created Time: 2017年12月25日 星期一 22时08分50秒
+ ************************************************************************/
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include "print"
+using namespace std;
+
--- stl/lambda1.cpp
+++ stl/lambda1.cpp
@@ -1,26 +1,26 @@
 /*************************************************************************
-    > File Name: labda1.cpp
+    > File Name: lambda1.cpp
     > Author: King
     > Mail: arturiapendragon_1@163.com 
-    > Created Time: 2017年12月03日 星期日 09时28分22秒
+    > Created Time: 2017年11月24日 星期五 22时18分39秒
  ************************************************************************/
 
 #include <iostream>
-#include <deque>
-#include <algorithm>
 using namespace std;
 
-int main()
+int
+main(void)
 {
-	deque<int> coll = {1, 3, 19, 5, 13, 7, 11, 2, 17};
+	int x = 0;
+	int y = 42;
 
-	int x=5;
-	int y=12;
-
-	auto pos = find_if(coll.cbegin(), coll.cend(), [=](int i) {
-				return i > x && i < y;
-			});
-
-	cout << "first elem >5 and <12: " << *pos << endl;
+	auto qqq = [x, &y] {
+		cout << "x: " << x << endl;
+		cout << "y: " << y << endl;
+		++y;
+	};
+	x = y = 77;
+	qqq();
+	qqq();
+	cout <<"final y: " << y << endl;
 }
-
