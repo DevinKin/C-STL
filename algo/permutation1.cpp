d0cc54234f4760b89d34eeb907c9bb6665545a7a
DevinKin <DevinKin@163.com>
2018-01-18 16:44:24 +0800

    permutation1

---
+++ algo/permutation1.cpp
@@ -0,0 +1,39 @@
+/*************************************************************************
+    > File Name: permutation1.cpp
+    > Author: King
+    > Mail: arturiapendragon_1@163.com 
+    > Created Time: 2018年01月18日 星期四 16时25分12秒
+ ************************************************************************/
+
+#include "algostuff.hpp"
+using namespace std;
+
+int main()
+{
+	vector<int> coll;
+	INSERT_ELEMENTS(coll, 1, 3);
+	PRINT_ELEMENTS(coll, "on entry: ");
+
+	// permute elements until they are sorted
+	// - runs through all permutations because the elements are sorted now
+	while (next_permutation(coll.begin(), coll.end())) {
+		PRINT_ELEMENTS(coll, " ");
+	}
+	PRINT_ELEMENTS(coll, "afterward: ");
+
+	// permute until descending sorted
+	// - this is the next permutation after ascending sorting
+	// - so the loop ends immediately
+	while (prev_permutation(coll.begin(), coll.end())) {
+		PRINT_ELEMENTS(coll, " ");
+	}
+	PRINT_ELEMENTS(coll, "now: ");
+
+	// permute elements until they are sorted in descending order
+	// - runs through all permutations because the elements are sorted in descending order now
+	while (prev_permutation(coll.begin(), coll.end())) {
+		PRINT_ELEMENTS(coll, " ");
+	}
+	PRINT_ELEMENTS(coll, "afterward: ");
+}
+
