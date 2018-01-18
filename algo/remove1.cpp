e4ee175c4b383a38e5f69d5a182eb56c0745aa60
DevinKin <DevinKin@163.com>
2018-01-18 11:50:15 +0800

    remove1

---
+++ algo/remove1.cpp
@@ -0,0 +1,38 @@
+/*************************************************************************
+    > File Name: remove1.cpp
+    > Author: King
+    > Mail: arturiapendragon_1@163.com 
+    > Created Time: 2018年01月18日 星期四 11时44分29秒
+ ************************************************************************/
+
+#include "algostuff.hpp"
+using namespace std;
+
+int main()
+{
+	vector<int> coll;
+
+	INSERT_ELEMENTS(coll, 2, 6);
+	INSERT_ELEMENTS(coll, 4, 9);
+	INSERT_ELEMENTS(coll, 1, 7);
+	PRINT_ELEMENTS(coll, "coll:	");
+
+	// remove all elements with value 5
+	vector<int>::iterator pos;
+	pos = remove(coll.begin(), coll.end(), 5);
+
+	PRINT_ELEMENTS(coll, "size not changed: ");
+
+	// erase the "removed" elements in the container
+	coll.erase(pos, coll.end());
+	PRINT_ELEMENTS(coll, "size changed: ");
+
+	// remove all elements less than 4
+	coll.erase(remove_if(coll.begin(), coll.end(),			//range
+						[](int elem){
+							return elem < 4;
+						}),
+			coll.end());
+	PRINT_ELEMENTS(coll, "<4 removed: ");
+
+}
